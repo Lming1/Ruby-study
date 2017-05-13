7067eafd3a1720af7b24e2439297e4ad42205a30
Lming1 <spear131121@gmail.com>
2017-05-13 14:33:22 +0000

    comment db add

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ActiveRecord::Base
+    # 여러 댓글을 가지고 있다.
+    has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512155754) do
+ActiveRecord::Schema.define(version: 20170513125236) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "post_id"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
